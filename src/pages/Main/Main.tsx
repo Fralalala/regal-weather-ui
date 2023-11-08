46d9d7370fcdb2a699d4c4a49fa0a38bb70fffa5
Rolando Morales Jr <sirol@Rolandos-MacBook-Air.local>
2023-11-08 06:01:22 -1000

    style: Added description details

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -5,6 +5,7 @@ import Tile from "../../components/Tile/Tile";
 const Main = () => {
   const [weather, setWeather] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [isCelsius, setIsCelsius] = useState(true);
 
   return (
     <div id="main">
@@ -16,9 +17,32 @@ const Main = () => {
         </div>
 
         <div id="description-column">
-          
-
-
+          <img
+            className="description-weather-icon"
+            src="https://bmcdn.nl/assets/weather-icons/v3.0/fill/svg/tornado.svg"
+          />
+          <div className="description">
+            <div>
+              34°
+              <div className="description-temperature">
+                °
+                <span
+                  className="celsius"
+                  style={{ textDecoration: isCelsius ? "underline" : "" }}
+                >
+                  C
+                </span>
+                <span style={{ color: "black" }}>|</span>°
+                <span
+                  className="farenheight"
+                  style={{ textDecoration: !isCelsius ? "underline" : "" }}
+                >
+                  F
+                </span>
+              </div>
+              <div className="location">Philippines, Metro Manila</div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
